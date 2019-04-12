5c8fd25fe535831a4ed46af8831d5e78c7272be8
Whanyerak <v.morin59@gmail.com>
2019-04-12 13:36:40 +0200

    Some improvements A lot of modifications to simulate a eCommerce real website. Product and orders management.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,24 @@
 class PagesController < ApplicationController
 
   def home
-	#url = 'http://localhost:3000/bankAccount/ext/?id=3'
-  end
+		@produits = Produit.all
+	end
 
   def achat
   	id_user = current_user.compte.to_s
+  	produit = Produit.where(id: params[:produit_id]).last
+  	prix = produit.prix.to_s
 
-  	url = 'http://localhost:3000/removeMoney/ext/?id='+ id_user +'&money=5'
-	response = RestClient.get(url)
-	@result = response.to_json 
+  	url = 'http://localhost:3000/removeMoney/ext/?id='+ id_user +'&money=' + prix
+		response = RestClient.get(url)
+		@result = response.to_json
+
+		#commande_create_path(:id_produit => produit.id)
+		@commande = Commande.new
+    @commande.id_client = current_user.id
+    @commande.id_produit = produit.id
+    @commande.date_achat = Date.today
+    @commande.save
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_11_215706) do
+ActiveRecord::Schema.define(version: 2019_04_12_084437) do
+
+  create_table "commandes", force: :cascade do |t|
+    t.integer "id_client"
+    t.integer "id_produit"
+    t.date "date_achat"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +30,14 @@ ActiveRecord::Schema.define(version: 2019_04_11_215706) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_models_on_email", unique: true
     t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
+  create_table "produits", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "prix"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
